d0ab83431c5f785b2c28803ddcd54ccd388eaa6e
Student <example@example.com>
2020-02-07 09:58:58 -0800

    1. third spec for the users age in Venus years failed

--- __tests__/super-galactic.test.js
+++ __tests__/super-galactic.test.js
@@ -5,8 +5,14 @@ describe("SuperGalactic", () => {
     const age = new SuperGalactic(23);
     expect(age.getAge()).toEqual(23)
   });
+
   test("should take user's input age and return their age in Mercury years", () => {
     const age = new SuperGalactic(23);
     expect(age.getMercuryAge()).toEqual(95.83)
+  });
+
+  test("should take user's input age and return their age in Venus years", () => {
+    const age = new SuperGalactic(23);
+    expect(age.getVenusAge()).toEqual(37.10)
   });
 })
