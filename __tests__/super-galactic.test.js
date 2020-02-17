afd59ef7853af56653366573fcd80b5b74067c78
Student <example@example.com>
2020-02-16 21:09:42 -0800

    1. reformatted spacing on js files 2. removed uneccessary methods 3. refactored methods to remove Number constructor

--- __tests__/super-galactic.test.js
+++ __tests__/super-galactic.test.js
@@ -1,48 +1,39 @@
 import { SuperGalactic } from '../src/super-galactic';
 
 describe("SuperGalactic", () => {
-  test("should take user's input age and return their age", () => {
-    const age = new SuperGalactic(23);
-    expect(age.getAgeInYears()).toEqual(23)
-  });
-
   test("should take user's input age and return their age in Mercury years", () => {
     const age = new SuperGalactic(23);
-    expect(age.getMercuryAgeInYears()).toEqual(95.83)
+    expect(age.getMercuryAgeInYears()).toEqual("95.83")
   });
 
   test("should take user's input age and return their age in Venus years", () => {
     const age = new SuperGalactic(23);
-    expect(age.getVenusAgeInYears()).toEqual(37.10)
+    expect(age.getVenusAgeInYears()).toEqual("37.10")
   });
 
   test("should take user's input age and return their age in Mars years", () => {
     const age = new SuperGalactic(23);
-    expect(age.getMarsAgeInYears()).toEqual(12.23)
+    expect(age.getMarsAgeInYears()).toEqual("12.23")
   });
 
   test("should take user's input age and return their age in Jupiter years", () => {
     const age = new SuperGalactic(23);
-    expect(age.getJupiterAgeInYears()).toEqual(1.94)
+    expect(age.getJupiterAgeInYears()).toEqual("1.94")
   });
 
   test("should take user's input age and return their age in Saturn years", () => {
     const age = new SuperGalactic(23);
-    expect(age.getSaturnAgeInYears()).toEqual(0.78)
+    expect(age.getSaturnAgeInYears()).toEqual("0.78")
   });
 
   test("should take user's input age and return their age in Uranus years", () => {
     const age = new SuperGalactic(23);
-    expect(age.getUranusAgeInYears()).toEqual(0.27)
+    expect(age.getUranusAgeInYears()).toEqual("0.27")
   });
 
   test("should take user's input age and return their age in Neptune years", () => {
     const age = new SuperGalactic(23);
-    expect(age.getNeptuneAgeInYears()).toEqual(0.14)
-  });
-  test("should take user's input age and return their age in Pluto years", () => {
-    const age = new SuperGalactic(23);
-    expect(age.getPlutoAgeInYears()).toEqual(0.09)
+    expect(age.getNeptuneAgeInYears()).toEqual("0.14")
   });
 
   test("should take user's input age for men and return their years to life expectancy or their years they lived past life expectancy on Earth", () => {
@@ -72,7 +63,7 @@ describe("SuperGalactic", () => {
 
 
 
- 
+
 
 
 })
--- src/super-galactic.js
+++ src/super-galactic.js
@@ -1,93 +1,89 @@
 export class SuperGalactic {
-    constructor(age, gender){
-        this.age = age,
-        this.gender = gender;
+  constructor(age, gender) {
+    this.age = age,
+      this.gender = gender;
+  }
+
+  getMercuryAgeInYears() {
+    return (this.age / .24).toFixed(2);
+  }
+
+  getVenusAgeInYears() {
+    return (this.age / .62).toFixed(2);
+  }
+
+  getMarsAgeInYears() {
+    return (this.age / 1.88).toFixed(2);
+  }
+
+  getJupiterAgeInYears() {
+    return (this.age / 11.86).toFixed(2);
+  }
+
+  getSaturnAgeInYears() {
+    return (this.age / 29.46).toFixed(2);
+  }
+
+  getUranusAgeInYears() {
+    return (this.age / 84.01).toFixed(2);
+  }
+
+  getNeptuneAgeInYears() {
+    return (this.age / 164.79).toFixed(2);
+  }
+
+  getPlutoAgeInYears() {
+    return (this.age / 248.59).toFixed(2);
+  }
+
+  lifeExpectancyOnEarth(age, gender) {
+    if (gender === "male") {
+      let men = 69.8;
+
+      if (age < men) {
+        return ("Years to life expectancy for you: " + (men - age).toFixed(2));
+      }
+      return "Years lived past life expectancy for you: " + (age - men).toFixed(2);
     }
+    else if (gender === "female") {
+      let women = 74.2;
 
-    getAgeInYears(){
-        return Number(this.age);
+      if (age < women) {
+        return ("Years to life expectancy for you: " + (women - age).toFixed(2));
+      }
+      return "Years lived past life expectancy for you: " + (age - women).toFixed(2);
     }
+    else {
+      let non_binary = 72;
 
-    getMercuryAgeInYears(){
-        return Number((this.age / .24).toFixed(2)); 
+      if (age < non_binary) {
+        return ("Years to life expectancy for you: " + (non_binary - age).toFixed(2));
+      }
+      return "Years lived past life expectancy for you: " + (age - non_binary).toFixed(2);
     }
+  }
+  lifeExpectancyOnMercury() {
+    let value = this.getMercuryAgeInYears();
+    return (this.lifeExpectancyOnEarth(value, this.gender));
 
-    getVenusAgeInYears(){
-        return Number((this.age / .62).toFixed(2)); 
-    }
+  }
 
-    getMarsAgeInYears(){
-        return Number((this.age / 1.88).toFixed(2)); 
-    }
+  lifeExpectancyOnVenus() {
+    let value = this.getVenusAgeInYears();
+    return this.lifeExpectancyOnEarth(value, this.gender);
+  }
 
-    getJupiterAgeInYears(){
-        return Number((this.age / 11.86).toFixed(2)); 
-    }
+  lifeExpectancyOnMars() {
+    let value = this.getMarsAgeInYears();
+    return this.lifeExpectancyOnEarth(value, this.gender);
+  }
 
-    getSaturnAgeInYears(){
-        return Number((this.age / 29.46).toFixed(2)); 
-    }
+  lifeExpectancyOnJupiter() {
+    let value = this.getJupiterAgeInYears();
+    return this.lifeExpectancyOnEarth(value, this.gender);
+  }
 
-    getUranusAgeInYears(){
-        return Number((this.age / 84.01).toFixed(2)); 
-    }
 
-    getNeptuneAgeInYears(){
-        return Number((this.age / 164.79).toFixed(2)); 
-    }
-
-    getPlutoAgeInYears(){
-        return Number((this.age / 248.59).toFixed(2)); 
-    }
-
-    lifeExpectancyOnEarth(age, gender){
-        if (gender === "male") {
-            let men = 69.8;
-
-            if (age < men){
-                return ("Years to life expectancy for you: " + (men - age).toFixed(2));
-            }
-            return "Years lived past life expectancy for you: " + (age - men).toFixed(2);
-        }
-        else if (gender === "female"){
-            let women = 74.2;
-
-            if (age < women){
-                return ("Years to life expectancy for you: " + (women - age).toFixed(2));
-            }
-            return "Years lived past life expectancy for you: " + (age - women).toFixed(2);
-        }
-        else {
-            let non_binary = 72;
-
-            if (age < non_binary){
-                return ("Years to life expectancy for you: " + (non_binary - age).toFixed(2));
-            }
-            return "Years lived past life expectancy for you: " + (age - non_binary).toFixed(2);
-        }
-    }
-    lifeExpectancyOnMercury(){
-        let value = this.getMercuryAgeInYears();
-        return (this.lifeExpectancyOnEarth(value, this.gender));
-        
-    }
-
-    lifeExpectancyOnVenus(){
-        let value = this.getVenusAgeInYears();
-        return this.lifeExpectancyOnEarth(value, this.gender);
-    }
-
-    lifeExpectancyOnMars(){
-        let value = this.getMarsAgeInYears();
-        return this.lifeExpectancyOnEarth(value, this.gender);
-    }
-
-    lifeExpectancyOnJupiter(){
-        let value = this.getJupiterAgeInYears();
-        return this.lifeExpectancyOnEarth(value, this.gender);
-    }
-
-    
 
 
 }
